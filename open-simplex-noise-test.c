a0989cb5569ff4a57f459eefc6b5fd463be3b876
Bram Stolk <b.stolk@gmail.com>
2014-11-02 19:54:32 -0800

    Use three octaves to generate test2d.png

--- open-simplex-noise-test.c
+++ open-simplex-noise-test.c
@@ -70,6 +70,7 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {
 	int x, y;
 	double value;
+	double v0,v1,v2; // values from different octaves.
 	uint32_t rgb;
 	uint32_t image2d[HEIGHT][WIDTH];
 	uint32_t image3d[HEIGHT][WIDTH];
@@ -80,7 +81,15 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 
 	for (y = 0; y < HEIGHT; y++) {
 		for (x = 0; x < WIDTH; x++) {
+#if defined(SINGLE_OCTAVE)
 			value = open_simplex_noise4(ctx, (double) x / FEATURE_SIZE, (double) y / FEATURE_SIZE, 0.0, 0.0);
+#else
+			// Use three octaves: frequency N, N/2 and N/4 with relative amplitudes 4:2:1.
+			v0 = open_simplex_noise4(ctx, (double) x / FEATURE_SIZE / 4, (double) y / FEATURE_SIZE / 4, 0.0, 0.0);
+			v1 = open_simplex_noise4(ctx, (double) x / FEATURE_SIZE / 2, (double) y / FEATURE_SIZE / 2, 0.0, 0.0);
+			v2 = open_simplex_noise4(ctx, (double) x / FEATURE_SIZE / 1, (double) y / FEATURE_SIZE / 1, 0.0, 0.0);
+			value = v0 * 4 / 7.0 + v1 * 2 / 7.0 + v2 * 1 / 7.0;
+#endif
 			rgb = 0x010101 * (uint32_t) ((value + 1) * 127.5);
 			image2d[y][x] = (0x0ff << 24) | (rgb);
 
