f70cfb609fb14ec4cdc6163ccce55f9b2cb11dee
Stephen M. Cameron <stephenmcameron@gmail.com>
2014-11-03 18:19:35 -0600

    fix incorrect function prototype in open-simplex-noise.h
    open_simplex_noise_init should be open_simplex_noise_init_perm

--- open-simplex-noise.h
+++ open-simplex-noise.h
@@ -22,10 +22,10 @@
 #include <stdint.h>
 
 struct osn_context;
- 
-void open_simplex_noise_init(uint16_t perm[], int nelements);
-void open_simplex_noise_free(struct osn_context *ctx);
+
 int open_simplex_noise(int64_t seed, struct osn_context **ctx);
+void open_simplex_noise_free(struct osn_context *ctx);
+int open_simplex_noise_init_perm(struct osn_context *ctx, int16_t p[], int nelements);
 double open_simplex_noise2(struct osn_context *ctx, double x, double y);
 double open_simplex_noise3(struct osn_context *ctx, double x, double y, double z);
 double open_simplex_noise4(struct osn_context *ctx, double x, double y, double z, double w);
